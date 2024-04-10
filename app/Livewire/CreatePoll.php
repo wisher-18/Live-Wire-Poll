27f302cb001a4d8cee6c871848c79f6b42850898
wisher-18 <johnnysonn10@gmail.com>
2024-04-10 23:58:27 +0530

    add livewire component, models and migrations

---
+++ app/Livewire/CreatePoll.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatePoll extends Component
+{
+    public $title;
+    public $options = ['First'];
+    public function render()
+    {
+        return view('livewire.create-poll');
+    }
+
+    public function addOption(){
+        $this->options[] = '';
+    }
+
+    public function removeOption($index){
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+}
---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,26 @@
+<div>
+<form>
+    <label for="title">Title:</label>
+ 
+    <input type="text" wire:model="title" /> 
+    Current Title: {{$title}}
+
+    <div class="mb-4 mt-4">
+        <button class="btn" wire:click.prevent="addOption">Add Option</button>
+    </div>
+
+    <div>
+        @foreach($options as $index => $option)
+            <div class="mb-4">
+               <label>Option {{$index+1}}</label>
+               <div class="flex gap-2">
+                <input type="text" wire:model="options.{{$index}}">
+                <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+            </div>
+            </div>
+            
+        @endforeach
+    </div>
+</form>
+
+</div>
